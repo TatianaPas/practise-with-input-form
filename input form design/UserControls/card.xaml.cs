3aee1abf74eab9659195abdc41de876c93bda5c9
Tatiana <tatjana.pasechnik@gmail.com>
2022-05-03 21:40:34 +1200

    Add project files.

---
+++ input form design/UserControls/card.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace input_form_design.UserControls
+{
+    /// <summary>
+    /// Interaction logic for card.xaml
+    /// </summary>
+    public partial class card : UserControl
+    {
+        public card()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+            {
+            get{return(string) GetValue(TitleProperty); }
+            set{ SetValue(TitleProperty, value); }
+   
+            }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(card));
+        public string Time
+        {
+            get { return (string)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register("Title", typeof(string), typeof(card));
+
+
+    public string Number
+    {
+        get { return (string)GetValue(NumberProperty); }
+        set { SetValue(NumberProperty, value); }
+
+    }
+
+    public static readonly DependencyProperty NumberProperty = DependencyProperty.Register("Number", typeof(string), typeof(card));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("isActive", typeof(bool), typeof(card));
+
+    }
+  
+}
---
+++ input form design/UserControls/myTextBox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace input_form_design.UserControls
+{
+    /// <summary>
+    /// Interaction logic for myTextBox.xaml
+    /// </summary>
+    public partial class myTextBox : UserControl
+    {
+        public myTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
